a99ba82f92d8e0195ef72d074672b776d21ff68c
unica001 <uniagent001@gmail.com>
2018-11-21 14:07:38 +0530

    Connect with event storyboard

---
+++ Carrier UTC/Module/Event/Controller/EventListViewC.swift
@@ -0,0 +1,30 @@
+//
+//  EventListViewC.swift
+//  Carrier UTC
+//
+//  Created by Shilpa Sharma on 21/11/18.
+//  Copyright © 2018 Indigo. All rights reserved.
+//
+
+import UIKit
+
+class EventListViewC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
